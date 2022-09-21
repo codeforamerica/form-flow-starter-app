9478a641bda39ddcfe2ab28388488bb06432959a
Ben Calegari <bcalegari@codeforamerica.org>
2022-09-21 18:12:03 -0400

    Minimal amount to show properties in actuator endpoint
    - Remember to swap out your home directory to pull in the library jar
      for gradle!!

--- src/main/java/org/formflowstartertemplate/app/AppApplication.java
+++ src/main/java/org/formflowstartertemplate/app/AppApplication.java
@@ -1,15 +1,15 @@
 package org.formflowstartertemplate.app;
 
-import formflow.library.config.FlowsConfigurationFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class AppApplication {
 
 	public static void main(String[] args) {
-		FlowsConfigurationFactory flowsConfigurationFactory = new FlowsConfigurationFactory();
 		SpringApplication.run(AppApplication.class, args);
 	}
 
