e1874ef2f8c690106bf8326a1b478e368f6a9976
Bethany Seeger <bseeger@codeforamerica.org>
2022-09-22 10:17:28 -0400

    Adds a component scan to explicitly pull in our form-flow lib beans.

--- src/main/java/org/formflowstartertemplate/app/AppApplication.java
+++ src/main/java/org/formflowstartertemplate/app/AppApplication.java
@@ -3,10 +3,12 @@ package org.formflowstartertemplate.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ComponentScan("formflow.library.config")
 public class AppApplication {
 
 	public static void main(String[] args) {
