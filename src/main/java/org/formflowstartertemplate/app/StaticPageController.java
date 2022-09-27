7ba35d9f4489f12b0845fc54e843325d51379e70
Bethany Seeger <bseeger@codeforamerica.org>
2022-09-27 09:50:53 -0400

    Adds javadocs from original codebase.
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>

--- src/main/java/org/formflowstartertemplate/app/StaticPageController.java
+++ src/main/java/org/formflowstartertemplate/app/StaticPageController.java
@@ -4,9 +4,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * A controller to render static pages that are not in any flow.
+ */
 @Controller
 public class StaticPageController {
 
+  /**
+   * Renders the website index page.
+   *
+   * @param httpSession The current HTTP session, not null
+   * @return the static page template
+   */
   @GetMapping("/")
   String getIndex(
       HttpSession httpSession
@@ -17,11 +26,21 @@ public class StaticPageController {
     return "index";
   }
 
+  /**
+   * Renders the website faq page.
+   *
+   * @return the static page template
+   */
   @GetMapping("/faq")
   String getFaq() {
     return "faq";
   }
 
+  /**
+   * Renders a utility page to show all icons and their names.
+   *
+   * @return the static page template
+   */
   @GetMapping("/icons")
   String getIcons() {
     return "/fragments/icons";
