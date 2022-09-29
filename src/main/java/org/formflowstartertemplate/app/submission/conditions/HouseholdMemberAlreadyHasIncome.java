f85ad3735eb3731ef52e844cd2e65c704ca6a894
Bethany Seeger <bseeger@codeforamerica.org>
2022-09-29 15:39:18 -0400

    updates to use the TemplateManager in templates to provide Conditions and Actions

--- src/main/java/org/formflowstartertemplate/app/submission/conditions/HouseholdMemberAlreadyHasIncome.java
+++ src/main/java/org/formflowstartertemplate/app/submission/conditions/HouseholdMemberAlreadyHasIncome.java
@@ -5,18 +5,14 @@ import formflow.library.data.Submission;
 import java.util.ArrayList;
 import java.util.Map;
 
-/**
- * This is an example of how one could create a Condition class.
- * It is referenced from the flows-config.yaml, but is specific to the UBI flow.
- */
-public class householdMemberAlreadyHasIncome implements Condition  {
+public class HouseholdMemberAlreadyHasIncome implements Condition  {
 
     @Override
-    public Boolean run(Submission submission, String householdMemberName) {
+    public Boolean run(Submission submission, String data) {
         if (submission.getInputData().containsKey("income")) {
             var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
             var memberItationOptional = incomeArr.stream().filter(entry ->
-                entry.get("householdMember").equals(householdMemberName)).findFirst();
+                entry.get("householdMember").equals(data)).findFirst();
             if (memberItationOptional.isPresent()) {
                 return (Boolean) memberItationOptional.get().get("isSubflowComplete");
             }
