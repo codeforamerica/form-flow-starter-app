11ef2f56c2053c7f7ad9d4df5e2a9c347cd53943
B. Seeger <bseeger@users.noreply.github.com>
2022-11-01 16:29:30 -0400

    Adds commit hash to index page's footer (#18)
    * Adds ability to get git information to display in UI, if wanted.
    * Removes unnecessary empty line additions.
    * adds back in commit date/time

--- src/main/java/org/formflowstartertemplate/app/StaticPageController.java
+++ src/main/java/org/formflowstartertemplate/app/StaticPageController.java
@@ -1,14 +1,21 @@
 package org.formflowstartertemplate.app;
 
+import java.util.HashMap;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * A controller to render static pages that are not in any flow.
  */
 @Controller
 public class StaticPageController {
+
+  @Autowired
+  GitProperties gitProperties;
 
   /**
    * Renders the website index page.
@@ -17,11 +24,16 @@ public class StaticPageController {
    * @return the static page template
    */
   @GetMapping("/")
-  String getIndex(HttpSession httpSession) {
+  ModelAndView getIndex(HttpSession httpSession) {
     // For dev, reset session if you visit home
     httpSession.invalidate();
 
-    return "index";
+    // provide a model so that we can pass the git commit hash to the footer, via the index page.
+    HashMap<String, Object> model = new HashMap<>();
+    model.put("codeCommitHashShort", gitProperties.getShortCommitId());
+    model.put("codeCommitDateTime", gitProperties.getCommitTime());
+
+    return new ModelAndView("index", model);
   }
 
   /**
