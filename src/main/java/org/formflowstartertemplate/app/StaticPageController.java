95a106b9d457637951769a6406862bed7e83236b
Bethany Seeger <bseeger@codeforamerica.org>
2023-02-08 09:21:43 -0500

    Tests out new yes and no button fragment. Not ready yet.

--- src/main/java/org/formflowstartertemplate/app/StaticPageController.java
+++ src/main/java/org/formflowstartertemplate/app/StaticPageController.java
@@ -42,8 +42,17 @@ public class StaticPageController {
    * @return the static page template
    */
   @GetMapping("/faq")
-  String getFaq() { return "faq"; }
+  String getFaq() {
+    return "faq";
+  }
 
   @GetMapping("/privacy")
-  String getPrivacy() { return "privacy"; }
+  String getPrivacy() {
+    return "privacy";
+  }
+
+  @GetMapping("/testPage")
+  String getTestPage() {
+    return "testPage";
+  }
 }
