0a9f346fbd24741e3fbf27933fd04f0eb283ec14
spokenbird <agonzalez@codeforamerica.org>
2023-03-21 16:36:27 -0700

    Add individual scenario smarty pages
    [#184695583]
    
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

---
+++ src/main/java/org/formflowstartertemplate/app/submission/conditions/SmartySuggestionNotFound.java
@@ -0,0 +1,12 @@
+package org.formflowstartertemplate.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+
+public class SmartySuggestionNotFound implements Condition {
+
+  @Override
+  public Boolean run(Submission submission) {
+    return !submission.getInputData().containsKey("residentialAddressStreetAddress1_validated");
+  }
+}
