207b744c95f3a03f55f2f785662170c83a2d772a
B. Seeger <bseeger@users.noreply.github.com>
2023-03-28 16:13:34 -0400

    Disables smarty in testing. Puts back in address validation test. (#106)
    * Disables smarty in testing
    * Puts in address validation test
    * Re-orders flow setup for address page.
    * Removes unused Condition file.
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/formflowstartertemplate/app/submission/conditions/HomeAddressValidationIsOff.java
+++ src/main/java/org/formflowstartertemplate/app/submission/conditions/HomeAddressValidationIsOff.java
@@ -2,13 +2,18 @@ package org.formflowstartertemplate.app.submission.conditions;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HomeAddressValidationIsOff implements Condition {
 
+  @Value("${form-flow.address-validation.disabled:false}")
+  private boolean isAddressValidationDisabled;
+
   @Override
   public Boolean run(Submission submission) {
-    return submission.getInputData().get("_validateresidentialAddress").equals("false");
+    boolean addrValidationOffAtFragmentLevel = submission.getInputData().get("_validateresidentialAddress").equals("false");
+    return isAddressValidationDisabled || addrValidationOffAtFragmentLevel;
   }
 }
