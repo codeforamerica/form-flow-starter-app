45322f68951739b70e56b41e7021b0e9b4d14e11
Alex Gonzalez <agonzalez@codeforamerica.org>
2023-04-24 14:18:42 -0700

    Upgrade starter app to spring boot 3.0 (#121)
    * Update javax calls to Jakarta
    * Global remove all thymeleaf fragment syntax that used single tick quotes
    * Allow flyway to clean in test
    * Delete unused firstPage

--- src/main/java/org/formflowstartertemplate/app/StaticPageController.java
+++ src/main/java/org/formflowstartertemplate/app/StaticPageController.java
@@ -1,6 +1,6 @@
 package org.formflowstartertemplate.app;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
