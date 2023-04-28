e33d8509c0bd5eedf75cf7569d8ad3782791ce11
spokenbird <agonzalez@codeforamerica.org>
2023-04-27 17:28:51 -0700

    Update DateOfBirthPreparer to use HashMap and fix Apt not showing

--- src/main/java/org/formflowstartertemplate/app/pdf/DateOfBirthPreparer.java
+++ src/main/java/org/formflowstartertemplate/app/pdf/DateOfBirthPreparer.java
@@ -4,20 +4,20 @@ import formflow.library.data.Submission;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
-import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DateOfBirthPreparer implements SubmissionFieldPreparer {
 
   @Override
-  public List<SubmissionField> prepareSubmissionFields(Submission submission) {
+  public Map<String, SubmissionField> prepareSubmissionFields(Submission submission) {
     boolean hasState = submission.getInputData().containsKey("residentialAddressState");
     if (hasState) {
       if (submission.getInputData().get("residentialAddressState").toString().equalsIgnoreCase("MI")) {
-        return List.of(new SingleField("birthDate", "XX/XX/XXXX", null));
+        return Map.of("birthDate", new SingleField("birthDate", "XX/XX/XXXX", null));
       }
     }
-    return List.of();
+    return Map.of();
   }
 }
