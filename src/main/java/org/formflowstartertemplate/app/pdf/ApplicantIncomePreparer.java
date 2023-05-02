61a3c05c4d6940e7b0e67c2e6bdc8f84b9ad6631
bencalegari <bcalegari@codeforamerica.org>
2023-05-02 16:34:28 -0700

    wip

--- src/main/java/org/formflowstartertemplate/app/pdf/ApplicantIncomePreparer.java
+++ src/main/java/org/formflowstartertemplate/app/pdf/ApplicantIncomePreparer.java
@@ -1,13 +1,16 @@
 package org.formflowstartertemplate.app.pdf;
 
 import formflow.library.data.Submission;
+import formflow.library.pdf.CheckboxField;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicantIncomePreparer implements SubmissionFieldPreparer {
@@ -15,26 +18,38 @@ public class ApplicantIncomePreparer implements SubmissionFieldPreparer {
   @Override
   public Map<String, SubmissionField> prepareSubmissionFields(Submission submission) {
     Map<String, SubmissionField> applicantIncomeFieldMap = new HashMap<>();
-    
-    boolean houseHoldhasIncome = submission.getInputData().containsKey("income");
-    
-    if (houseHoldhasIncome) {
-      ArrayList<Map<String, Object>> householdIncomeSubflow = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
-      
-      boolean applicantHasIncome = householdIncomeSubflow.stream().anyMatch(iteration -> iteration.get("householdMember").equals("applicant"));
-      
+
+    boolean householdHasIncome = submission.getInputData().containsKey("income");
+
+    if (householdHasIncome) {
+      ArrayList<Map<String, Object>> householdIncomeSubflow = (ArrayList<Map<String, Object>>) submission.getInputData()
+          .get("income");
+
+      boolean applicantHasIncome = householdIncomeSubflow.stream()
+          .anyMatch(iteration -> iteration.get("householdMember").equals("applicant"));
+
       if (applicantHasIncome) {
         applicantIncomeFieldMap.put("applicantReceivesIncome", new SingleField("applicantReceivesIncome", "Yes", null));
 
-        Map<String, Object> applicantSubflowIteration = householdIncomeSubflow.stream()
-            .filter(iteration -> iteration.get("householdMember").equals("applicant")).findFirst().orElse(null);
-        
-        applicantSubflowIteration.get("incomeTypes[]").forEach(incomeType -> {
-          applicantIncomeFieldMap.put("applicantIncomeType" + incomeType, new SingleField("applicantIncomeType" + incomeType, "Yes", null));
+        Map<String, Object> applicantSubflowIteration = householdIncomeSubflow.stream().filter(iteration ->
+            iteration.get("householdMember").equals("applicant")).findFirst().orElse(null);
+        List<String> incomeTypes = (List<String>) applicantSubflowIteration.get("incomeTypes[]");
+        applicantIncomeFieldMap.put(
+            "applicantIncomeTypes",
+            new CheckboxField("applicantIncomeTypes", incomeTypes, null));
+        incomeTypes.forEach(incomeType -> {
+          String inputName = incomeType + "Amount";
+          applicantIncomeFieldMap.put(
+                  inputName,
+              new SingleField(inputName, applicantSubflowIteration.get(inputName).toString(), null));
         });
+
       } else {
         applicantIncomeFieldMap.put("applicantReceivesIncome", new SingleField("applicantReceivesIncome", "No", null));
       }
     }
+
+
+    return applicantIncomeFieldMap;
   }
 }
