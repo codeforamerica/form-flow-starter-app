f470f7b16194df5f5d244890e53a0bcf0c00c165
bencalegari <bcalegari@codeforamerica.org>
2023-05-23 12:30:15 -0700

    Add https forwarding
    [#185023914]

--- src/main/java/org/formflowstartertemplate/app/config/SecurityConfiguration.java
+++ src/main/java/org/formflowstartertemplate/app/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +16,10 @@ public class SecurityConfiguration {
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.formLogin().disable();
     return httpSecurity.build();
+  }
+
+  @Bean
+  public ForwardedHeaderFilter forwardedHeaderFilter() {
+    return new ForwardedHeaderFilter();
   }
 }
