529aa7f3f41ae2bb7ab82edcf8922c6e0235999f
spokenbird <agonzalez@codeforamerica.org>
2023-06-01 18:27:27 -0700

    WIP

--- src/main/java/org/formflowstartertemplate/app/pdf/HouseholdMemberReceivesIncomePreparer.java
+++ src/main/java/org/formflowstartertemplate/app/pdf/HouseholdMemberReceivesIncomePreparer.java
@@ -5,6 +5,7 @@ import formflow.library.pdf.PdfMap;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
@@ -15,22 +16,33 @@ public class HouseholdMemberReceivesIncomePreparer implements SubmissionFieldPre
   @Override
   public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, Map<String, Object> data, PdfMap pdfMap) {
     Map<String, SubmissionField> householdMembersReceivingIncomeMap = new HashMap<>();
-    int maxIterations = pdfMap.getSubflowInfo().get("householdAndIncome").getTotalIterations();
+    int totalIterations = pdfMap.getSubflowInfo().get("householdAndIncome").getTotalIterations();
     
+    boolean hasHousehold = submission.getInputData().containsKey("hasHousehold") &&
+        submission.getInputData().get("hasHousehold").equals("true");
     boolean householdHasIncome = submission.getInputData().containsKey("income");
     
-    if (householdHasIncome) {
-      for (int index = 1; index <= maxIterations; index++) {
-        if (data.containsKey("incomeTypes_" + index + "[]")) {
+    
+    if (hasHousehold && householdHasIncome) {
+      ArrayList<Map<String, Object>> incomeSubflow = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
+      ArrayList<Map<String, Object>> houseHoldSubflow = (ArrayList<Map<String, Object>>) submission.getInputData().get("household");
+      
+      int maxIterations = Math.min(houseHoldSubflow.size(), totalIterations);
+      
+      for (int i = 0; i < maxIterations; i++) {
+        String householdMemberFullName = houseHoldSubflow.get(i).get("householdMemberFirstName").toString() 
+            + " " + houseHoldSubflow.get(i).get("householdMemberLastName").toString();
+        if (incomeSubflow.stream().anyMatch(iteration -> iteration.get("householdMember").equals(householdMemberFullName))) {
           householdMembersReceivingIncomeMap.put(
-              "householdMemberReceivesIncome_" + index,
-              new SingleField("householdMemberReceivesIncome_" + index, "Yes", null));
+              "householdMemberReceivesIncome_" + i,
+              new SingleField("householdMemberReceivesIncome_" + i, "Yes", null));
         } else {
           householdMembersReceivingIncomeMap.put(
-              "householdMemberReceivesIncome_" + index,
-              new SingleField("householdMemberReceivesIncome_" + index, "No", null));
+              "householdMemberReceivesIncome_" + i,
+              new SingleField("householdMemberReceivesIncome_" + i, "No", null));
         }
       }
+
     }
     
    return householdMembersReceivingIncomeMap;
