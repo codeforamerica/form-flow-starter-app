75014db84f951c6c76bfb6ceaa71de501619e305
spokenbird <agonzalez@codeforamerica.org>
2023-06-02 14:37:15 -0700

    Working but should write tests test?

--- src/main/java/org/formflowstartertemplate/app/pdf/HouseholdMemberReceivesIncomePreparer.java
+++ src/main/java/org/formflowstartertemplate/app/pdf/HouseholdMemberReceivesIncomePreparer.java
@@ -1,6 +1,7 @@
 package org.formflowstartertemplate.app.pdf;
 
 import formflow.library.data.Submission;
+import formflow.library.pdf.CheckboxField;
 import formflow.library.pdf.PdfMap;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
@@ -15,36 +16,60 @@ public class HouseholdMemberReceivesIncomePreparer implements SubmissionFieldPre
 
   @Override
   public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, Map<String, Object> data, PdfMap pdfMap) {
-    Map<String, SubmissionField> householdMembersReceivingIncomeMap = new HashMap<>();
-    int totalIterations = pdfMap.getSubflowInfo().get("householdAndIncome").getTotalIterations();
-    
+    Map<String, SubmissionField> householdMemberSubmissionFields = new HashMap<>();
+
     boolean hasHousehold = submission.getInputData().containsKey("hasHousehold") &&
         submission.getInputData().get("hasHousehold").equals("true");
     boolean householdHasIncome = submission.getInputData().containsKey("income");
-    
-    
+
     if (hasHousehold && householdHasIncome) {
       ArrayList<Map<String, Object>> incomeSubflow = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
-      ArrayList<Map<String, Object>> houseHoldSubflow = (ArrayList<Map<String, Object>>) submission.getInputData().get("household");
-      
-      int maxIterations = Math.min(houseHoldSubflow.size(), totalIterations);
-      
+      ArrayList<Map<String, Object>> houseHoldSubflow = (ArrayList<Map<String, Object>>) submission.getInputData()
+          .get("household");
+
+      int maxIterations = Math.min(houseHoldSubflow.size(), pdfMap.getSubflowInfo().get("householdAndIncome").getTotalIterations());
+
       for (int i = 0; i < maxIterations; i++) {
-        String householdMemberFullName = houseHoldSubflow.get(i).get("householdMemberFirstName").toString() 
-            + " " + houseHoldSubflow.get(i).get("householdMemberLastName").toString();
-        if (incomeSubflow.stream().anyMatch(iteration -> iteration.get("householdMember").equals(householdMemberFullName))) {
-          householdMembersReceivingIncomeMap.put(
-              "householdMemberReceivesIncome_" + i,
-              new SingleField("householdMemberReceivesIncome_" + i, "Yes", null));
+        String householdMemberFirstName = houseHoldSubflow.get(i).get("householdMemberFirstName").toString();
+        String householdMemberLastName = houseHoldSubflow.get(i).get("householdMemberLastName").toString();
+        String householdMemberRelationship = houseHoldSubflow.get(i).get("householdMemberRelationship").toString();
+        String householdMemberRecentlyMovedToUs = houseHoldSubflow.get(i).get("householdMemberRecentlyMovedToUS").toString();
+        int iterationNumber = i + 1;
+        householdMemberSubmissionFields.putAll(Map.of(
+            "householdMemberFirstName_" + iterationNumber, new SingleField("householdMemberFirstName_" + iterationNumber, householdMemberFirstName, null),
+            "householdMemberLastName_" + iterationNumber, new SingleField("householdMemberLastName_" + iterationNumber, householdMemberLastName, null),
+            "householdMemberRelationship_" + iterationNumber,
+            new SingleField("householdMemberRelationship_" + iterationNumber, householdMemberRelationship, null),
+            "householdMemberRecentlyMovedToUS_" + iterationNumber,
+            new SingleField("householdMemberRecentlyMovedToUS_" + iterationNumber, householdMemberRecentlyMovedToUs, null)
+        ));
+
+        Map<String, Object> householdMember = incomeSubflow.stream().filter(
+                iteration -> iteration.get("householdMember").equals(householdMemberFirstName + " " + householdMemberLastName))
+            .findFirst().orElse(null);
+
+        if (householdMember != null) {
+          householdMemberSubmissionFields.put("householdMemberReceivesIncome_" + iterationNumber,
+              new SingleField("householdMemberReceivesIncome_" + iterationNumber, "Yes", null));
+
+          ArrayList<String> incomeTypes = (ArrayList<String>) householdMember.get("incomeTypes[]");
+          householdMemberSubmissionFields.put("incomeTypes_" + iterationNumber,
+              new CheckboxField("incomeTypes_" + iterationNumber, incomeTypes, null));
+
+          final int finalIterationNumber = iterationNumber;
+          incomeTypes.forEach(incomeType -> {
+            String incomeAmount = householdMember.get(incomeType + "Amount").toString();
+            householdMemberSubmissionFields.put(incomeType + "Amount_" + finalIterationNumber,
+                new SingleField(incomeType + "Amount_" + finalIterationNumber, incomeAmount, null));
+          });
+
         } else {
-          householdMembersReceivingIncomeMap.put(
-              "householdMemberReceivesIncome_" + i,
-              new SingleField("householdMemberReceivesIncome_" + i, "No", null));
+          householdMemberSubmissionFields.put("householdMemberReceivesIncome_" + iterationNumber,
+              new SingleField("householdMemberReceivesIncome_" + iterationNumber, "No", null));
         }
       }
-
     }
-    
-   return householdMembersReceivingIncomeMap;
+
+    return householdMemberSubmissionFields;
   }
 }
