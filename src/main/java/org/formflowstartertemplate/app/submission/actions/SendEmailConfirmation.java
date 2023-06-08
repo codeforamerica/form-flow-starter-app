b5adf2aaa553cbfe70ae6f14558517bae45f5cdf
Chibuisi Enyia <cenyia@codeforamerica.org>
2023-06-08 13:15:33 -0500

    Added an afterSaveAction for SendEmailConfirmation.  Used the SendEma… (#148)
    * Added an afterSaveAction for SendEmailConfirmation.  Used the SendEmailConfirmation to send an email after User signs name.
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>

---
+++ src/main/java/org/formflowstartertemplate/app/submission/actions/SendEmailConfirmation.java
@@ -0,0 +1,74 @@
+package org.formflowstartertemplate.app.submission.actions;
+
+
+import formflow.library.config.submission.Action;
+import formflow.library.data.Submission;
+import formflow.library.email.MailgunEmailClient;
+import formflow.library.pdf.PdfService;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SendEmailConfirmation implements Action {
+
+  @Autowired
+  MessageSource messageSource;
+  @Autowired
+  private PdfService pdfService;
+  @Autowired
+  private MailgunEmailClient mailgunEmailClient;
+
+  @Value("${form-flow.flow.ubi.email.confirmation.cc:}")
+  private List<String> emailToCc;
+
+  @Value("${form-flow.flow.ubi.email.confirmation.bcc:}")
+  private List<String> emailToBcc;
+
+  public void run(Submission submission) {
+    ArrayList<String> howToContactYou = (ArrayList<String>) submission.getInputData().get("howToContactYou[]");
+    Boolean agreesToEmailContact = howToContactYou.stream().anyMatch(contactType -> contactType.equals("email"));
+    if (!agreesToEmailContact) {
+      return;
+    }
+    String recipientEmail = (String) submission.getInputData().get("email");
+    if (recipientEmail == null || recipientEmail.isBlank()) {
+      return;
+    }
+    String emailSubject = messageSource.getMessage("email.subject", null, null);
+    Object[] args = new Object[]{submission.getId().toString()};
+    String emailBody = messageSource.getMessage("email.body", args, null);
+
+    Boolean requireTls = Boolean.TRUE;
+    List<File> pdfs = new ArrayList<File>();
+    try {
+      String generateStringPrefixName = pdfService.generatePdfName(submission);
+      File pdf = File.createTempFile(generateStringPrefixName, ".pdf");
+      byte[] pdfByteArray = pdfService.getFilledOutPDF(submission);
+      FileOutputStream fos = new FileOutputStream(pdf);
+      fos.write(pdfByteArray);
+      fos.flush();
+      pdfs.add(pdf);
+      mailgunEmailClient.sendEmail(
+          emailSubject,
+          recipientEmail,
+          emailToCc,
+          emailToBcc,
+          emailBody,
+          pdfs,
+          requireTls
+      );
+      pdf.delete();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
