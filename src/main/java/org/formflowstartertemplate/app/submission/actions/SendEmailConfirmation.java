ed5a60e9fe31ea632c6957671a68fc6c2d3bd6c7
Chibuisi Enyia <cenyia@codeforamerica.org>
2023-06-13 17:55:06 -0500

    Generate and send next steps email (#157)
    Added MessageResponse to SendEmailConfirmation
    
    #184899394
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/formflowstartertemplate/app/submission/actions/SendEmailConfirmation.java
+++ src/main/java/org/formflowstartertemplate/app/submission/actions/SendEmailConfirmation.java
@@ -1,6 +1,7 @@
 package org.formflowstartertemplate.app.submission.actions;
 
 
+import com.mailgun.model.message.MessageResponse;
 import formflow.library.config.submission.Action;
 import formflow.library.data.Submission;
 import formflow.library.email.MailgunEmailClient;
@@ -9,6 +10,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,12 +45,41 @@ public class SendEmailConfirmation implements Action {
     if (recipientEmail == null || recipientEmail.isBlank()) {
       return;
     }
+    Boolean requireTls = Boolean.TRUE;
+
     String emailSubject = messageSource.getMessage("email.subject", null, null);
     Object[] args = new Object[]{submission.getId().toString()};
     String emailBody = messageSource.getMessage("email.body", args, null);
-
-    Boolean requireTls = Boolean.TRUE;
     List<File> pdfs = new ArrayList<File>();
+    MessageResponse response = generatePDFEmail(recipientEmail, emailSubject, emailToCc, emailToBcc, emailBody, pdfs, requireTls, submission);
+
+    String nextStepsSubject = messageSource.getMessage("next-steps-email.subject", null, null);
+    String nextStepsBody = messageSource.getMessage("next-steps-email.body", null, null);
+    pdfs = new ArrayList<File>();
+    MessageResponse nextStepsResponse = mailgunEmailClient.sendEmail(
+        nextStepsSubject,
+        recipientEmail,
+        Collections.emptyList(),
+        Collections.emptyList(),
+        nextStepsBody,
+        pdfs,
+        requireTls
+    );
+    Boolean confirmationWasQueued = nextStepsResponse.getMessage().contains("Queued. Thank you.");
+    log.info("Mailgun MessageResponse confirms message was queued (true/false): " + confirmationWasQueued);
+    submission.getInputData().put("confirmationEmailQueued", confirmationWasQueued.toString());
+    submission.setInputData(submission.getInputData());
+  }
+
+  public MessageResponse generatePDFEmail(
+      String recipientEmail,
+      String emailSubject,
+      List<String> emailToCc,
+      List<String> emailToBcc,
+      String emailBody,
+      List<File>pdfs,
+      Boolean requireTls,
+      Submission submission){
     try {
       String generateStringPrefixName = pdfService.generatePdfName(submission);
       File pdf = File.createTempFile(generateStringPrefixName, ".pdf");
@@ -57,7 +88,8 @@ public class SendEmailConfirmation implements Action {
       fos.write(pdfByteArray);
       fos.flush();
       pdfs.add(pdf);
-      mailgunEmailClient.sendEmail(
+      MessageResponse response;
+      response = mailgunEmailClient.sendEmail(
           emailSubject,
           recipientEmail,
           emailToCc,
@@ -67,6 +99,7 @@ public class SendEmailConfirmation implements Action {
           requireTls
       );
       pdf.delete();
+      return response;
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
