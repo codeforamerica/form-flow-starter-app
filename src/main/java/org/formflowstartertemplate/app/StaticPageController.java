23ff11e2b71419797c4b2a8b3655c35bc4cd4fce
Cypress Borg <cborg@codeforamerica.org>
2023-07-11 12:27:54 -0500

    Add static page screen name and more es messages

--- src/main/java/org/formflowstartertemplate/app/StaticPageController.java
+++ src/main/java/org/formflowstartertemplate/app/StaticPageController.java
@@ -1,8 +1,12 @@
 package org.formflowstartertemplate.app;
 
 import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * A controller to render static pages that are not in any flow.
@@ -17,10 +21,13 @@ public class StaticPageController {
    * @return the static page template
    */
   @GetMapping("/")
-  String getIndex(HttpSession httpSession) {
+  ModelAndView getIndex(HttpSession httpSession) {
     httpSession.invalidate(); // For dev, reset session if you visit home
-    return "index";
-  }
+
+    Map<String, Object> model = new HashMap<>();
+    model.put("screen", "/");
+
+    return new ModelAndView("index", model);  }
 
   /**
    * Renders the website faq page.
@@ -28,12 +35,23 @@ public class StaticPageController {
    * @return the static page template
    */
   @GetMapping("/faq")
-  String getFaq() {
-    return "faq";
+  ModelAndView getFaq() {
+    Map<String, Object> model = new HashMap<>();
+    model.put("screen", "faq");
+
+    return new ModelAndView("faq", model);
   }
 
+  /**
+   * Renders the website privacy page.
+   *
+   * @return the static page template
+   */
   @GetMapping("/privacy")
-  String getPrivacy() {
-    return "privacy";
+  ModelAndView getPrivacy() {
+    Map<String, Object> model = new HashMap<>();
+    model.put("screen", "privacy");
+
+    return new ModelAndView("privacy", model);
   }
 }
