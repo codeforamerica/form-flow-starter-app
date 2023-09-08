52bb1812947444ddde08ce90801c53b71db78432
Bethany Seeger <bseeger@codeforamerica.org>
2023-09-08 15:18:02 -0400

    Adds a profile of test to one of our FlyConfig files

--- src/test/java/org/formflowstartertemplate/app/config/FlywayConfiguration.java
+++ src/test/java/org/formflowstartertemplate/app/config/FlywayConfiguration.java
@@ -3,8 +3,10 @@ package org.formflowstartertemplate.app.config;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("test")
 public class FlywayConfiguration {
 
   @Bean
