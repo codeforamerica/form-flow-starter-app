776af57866627258ba91377764658e8359a4453e
Martha Pidcock <mpidcock@codeforamerica.org>
2023-10-18 18:18:43 -0700

    Redirect to disabledFlowRedirect when flow is disabled

--- src/main/java/org/formflowstartertemplate/app/config/DisabledFlowInterceptorConfiguration.java
+++ src/main/java/org/formflowstartertemplate/app/config/DisabledFlowInterceptorConfiguration.java
@@ -2,12 +2,15 @@ package org.formflowstartertemplate.app.config;
 
 import java.util.List;
 
-import formflow.library.ScreenController;
+import formflow.library.config.FlowConfiguration;
 import org.formflowstartertemplate.app.interceptors.DisabledFlowInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.formflowstartertemplate.app.interceptors.DisabledFlowInterceptor.PATH_FORMAT;
 
 /***
  * Adds DisabledFlowInterceptorConfiguration to the Interceptor registry.
@@ -15,6 +18,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class DisabledFlowInterceptorConfiguration implements WebMvcConfigurer {
 
+  @Autowired
+  List<FlowConfiguration> flowConfigurations;
   private final String[] disabledFlows;
 
   public DisabledFlowInterceptorConfiguration(@Value("${form-flow.disabled-flows}") String[] disabledFlows) {
@@ -23,7 +28,7 @@ public class DisabledFlowInterceptorConfiguration implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new DisabledFlowInterceptor(disabledFlows))
-        .addPathPatterns(List.of(ScreenController.FLOW + "/" + ScreenController.FLOW_SCREEN_PATH));
+    registry.addInterceptor(new DisabledFlowInterceptor(flowConfigurations, disabledFlows))
+        .addPathPatterns(List.of(PATH_FORMAT));
   }
 }
--- src/main/java/org/formflowstartertemplate/app/interceptors/DisabledFlowInterceptor.java
+++ src/main/java/org/formflowstartertemplate/app/interceptors/DisabledFlowInterceptor.java
@@ -1,11 +1,20 @@
 package org.formflowstartertemplate.app.interceptors;
 
+import formflow.library.ScreenController;
+import formflow.library.config.FlowConfiguration;
+import formflow.library.exceptions.LandmarkNotSetException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
@@ -15,19 +24,49 @@ import org.springframework.web.servlet.HandlerInterceptor;
 // TODO: once format of value is locked down, make an applicable conditional
 //@ConditionalOnProperty(name = "form-flow.disabled-flows", havingValue = "*")
 public class DisabledFlowInterceptor implements HandlerInterceptor, Ordered {
+  public static final String PATH_FORMAT = ScreenController.FLOW + "/" + ScreenController.FLOW_SCREEN_PATH;
+
+  List<FlowConfiguration> flowConfigurations;
   String[] disabledFlows;
 
   public DisabledFlowInterceptor() {
     this.disabledFlows = new String[0];
   }
 
-  public DisabledFlowInterceptor(String[] disabledFlows) {
+  public DisabledFlowInterceptor(List<FlowConfiguration> flowConfigurations, String[] disabledFlows) {
+    this.flowConfigurations = flowConfigurations;
     this.disabledFlows = disabledFlows;
   }
 
   @Override
-  public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+  public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws LandmarkNotSetException {
     System.out.println("Disabled flows is equal to: " + Arrays.toString(disabledFlows));
+
+    Map<String, String> parsedUrl = new AntPathMatcher().extractUriTemplateVariables(PATH_FORMAT, request.getRequestURI());
+    String requestedFlow = parsedUrl.get("flow");
+    if (Arrays.asList(disabledFlows).contains(requestedFlow)) {
+      FlowConfiguration flowConfiguration = flowConfigurations.stream()
+              .filter(fc -> fc.getName().equals(parsedUrl.get("flow")))
+              .findFirst()
+              .orElse(null);
+
+      if (flowConfiguration == null ||
+              flowConfiguration.getLandmarks() == null ||
+              flowConfiguration.getLandmarks().getDisabledFlowRedirect() == null) {
+        throw new LandmarkNotSetException(
+                "Flow %s has been disabled with no 'disabledFlowRedirect'".formatted(requestedFlow));
+      }
+
+      String disabledFlowRedirect = flowConfiguration.getLandmarks().getDisabledFlowRedirect();
+      System.out.printf("Redirecting to %s - flow %s is disabled%n", disabledFlowRedirect, requestedFlow);
+
+      try {
+        response.sendRedirect(disabledFlowRedirect);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      return false;
+    }
     return true;
   }
 
