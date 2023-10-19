4d28ccd4b7f176e5b5d3a0012b61eb6ab35eb635
chibuisienyia <cenyia@codeforamerica.org>
2023-10-19 17:14:17 -0500

    shell scrip conditional logic to identify arm64 systems for node installs #186152311
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>

--- scripts/check_for_npm.sh
+++ scripts/check_for_npm.sh
@@ -3,8 +3,16 @@ set -e
 echo '--- checkForNPM ---'
 
 if ! npm --version | grep -q "^[0-9]"; then
+  echo $(uname -a)
+  echo $(uname -m)
   echo '🔴 npm is not installed, installing node now.'
-  brew install node
+  if [ "$(uname -m)" == "x86_64" ];then
+    echo 'ARM64 system'
+    arch -arm64 brew install node
+  else
+    echo 'NonARM64 system'
+    brew install node
+  fi
 else
   echo '🟢 npm is installed.'
 fi
