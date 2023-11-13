7dbedc75ae9d7f5653c879f96f2c6e6e289c8aa5
Cypress Borg <cborg@codeforamerica.org>
2023-11-13 17:19:10 -0600

    Add journey tests to make sure the right css/js assets are loaded

--- src/test/java/org/formflowstartertemplate/app/journeys/DesignSystemCfaUswdsJourneyTest.java
+++ src/test/java/org/formflowstartertemplate/app/journeys/DesignSystemCfaUswdsJourneyTest.java
@@ -1,6 +1,7 @@
 package org.formflowstartertemplate.app.journeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
@@ -10,13 +11,13 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 
 @Tag("staticPagesJourney")
-@Disabled
 @TestPropertySource(properties = {
     "form-flow.design-system.name=cfa-uswds",
 })
@@ -33,14 +34,23 @@ public class DesignSystemCfaUswdsJourneyTest extends AbstractBasePageTest {
   void staticPagesJourney() {
     assertThat(testPage.getTitle()).isEqualTo("Apply for UBI payments easily online.");
 
-    WebElement link = driver.findElement(By.cssSelector("link[href*='/assets/css/styles.css']"));
-    assertThat(link).isNotNull();
-    String hrefValue = link.getAttribute("href");
-    assertThat(hrefValue.contains("/assets/css/styles.css")).isTrue();
+    var cfaUswdsLinkName = "styles.css";
+    WebElement cfaUswdsLink = driver.findElement(By.cssSelector("link[href*='" + cfaUswdsLinkName + "']"));
+    assertThat(cfaUswdsLink).isNotNull();
+    assertThat(cfaUswdsLink.getAttribute("href").contains(cfaUswdsLinkName)).isTrue();
 
-    WebElement script = driver.findElement(By.cssSelector("script[src*='/assets/js/scripts.js']"));
-    assertThat(script).isNotNull();
-    String srcValue = script.getAttribute("src");
-    assertThat(srcValue.contains("/assets/js/scripts.js")).isTrue();
+    var cfaUswdsScriptName = "scripts.js";
+    WebElement cfaUswdsScript = driver.findElement(By.cssSelector("script[src*='" + cfaUswdsScriptName +"']"));
+    assertThat(cfaUswdsScript).isNotNull();
+    assertThat(cfaUswdsScript.getAttribute("src").contains(cfaUswdsScriptName)).isTrue();
+
+    var customLinkName = "custom.css";
+    assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> driver.findElement(By.cssSelector("link[href*='" + customLinkName + "']")));
+
+    var honeycrispLinkName = "honeycrisp.min.css";
+    assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> driver.findElement(By.cssSelector("link[href*='" + honeycrispLinkName + "']")));
+
+    var honeycrispScriptName = "honeycrisp.min.js";
+    assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> driver.findElement(By.cssSelector("script[src*='" + honeycrispScriptName +"']")));
   }
 }
--- src/test/java/org/formflowstartertemplate/app/journeys/DesignSystemHoneycrispJourneyTest.java
+++ src/test/java/org/formflowstartertemplate/app/journeys/DesignSystemHoneycrispJourneyTest.java
@@ -1,6 +1,7 @@
 package org.formflowstartertemplate.app.journeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.formflowstartertemplate.app.utils.AbstractBasePageTest;
 import org.formflowstartertemplate.app.utils.Page;
@@ -8,13 +9,13 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 
 @Tag("staticPagesJourney")
-@Disabled
 @TestPropertySource(properties = {
     "form-flow.design-system.name=honeycrisp",
 })
@@ -31,14 +32,22 @@ public class DesignSystemHoneycrispJourneyTest extends AbstractBasePageTest {
   void staticPagesJourney() {
     assertThat(testPage.getTitle()).isEqualTo("Apply for UBI payments easily online.");
 
-    WebElement link = driver.findElement(By.cssSelector("link[href*='/assets/css/custom.css']"));
-    assertThat(link).isNotNull();
-    String hrefValue = link.getAttribute("href");
-    assertThat(hrefValue.contains("/assets/css/styles.css")).isTrue();
+    WebElement customLink = driver.findElement(By.cssSelector("link[href*='custom.css']"));
+    assertThat(customLink).isNotNull();
+    assertThat(customLink.getAttribute("href").contains("custom.css")).isTrue();
 
-    WebElement script = driver.findElement(By.cssSelector("script[src*='/assets/js/scripts.js']"));
-    assertThat(script).isNotNull();
-    String srcValue = script.getAttribute("src");
-    assertThat(srcValue.contains("/assets/js/scripts.js")).isTrue();
+    WebElement honeycrispLink = driver.findElement(By.cssSelector("link[href*='honeycrisp.min.css']"));
+    assertThat(honeycrispLink).isNotNull();
+    assertThat(honeycrispLink.getAttribute("href").contains("honeycrisp.min.css")).isTrue();
+
+    WebElement honeycrispScript = driver.findElement(By.cssSelector("script[src*='honeycrisp.min.js']"));
+    assertThat(honeycrispScript).isNotNull();
+    assertThat(honeycrispScript.getAttribute("src").contains("honeycrisp.min.js")).isTrue();
+
+    var cfaUswdsLinkName = "styles.css";
+    assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> driver.findElement(By.cssSelector("link[href*='" + cfaUswdsLinkName + "']")));
+
+    var cfaUswdsScriptName = "scripts.js";
+    assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> driver.findElement(By.cssSelector("script[src*='" + cfaUswdsScriptName +"']")));
   }
 }
